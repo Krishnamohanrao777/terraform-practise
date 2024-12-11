e79dd08eee561e87bf789af01b3413948644d163
akash <akashpinnenti5@gmail.com>
2024-12-11 16:12:34 +0530

    private subnet

--- day-1-basic_code/main.tf
+++ day-1-basic_code/main.tf
@@ -12,12 +12,20 @@ resource "azurerm_virtual_network" "vnet" {
   resource_group_name = azurerm_resource_group.RG.name
 }
 
-# Create a subnet
+# Create a public subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "public-subnet"
   resource_group_name  = azurerm_resource_group.RG.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
+}
+
+# Create a private subnet
+resource "azurerm_subnet" "private_subnet" {
+  name                 = "private-subnet"
+  resource_group_name  = azurerm_resource_group.RG.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Create a public IP address
